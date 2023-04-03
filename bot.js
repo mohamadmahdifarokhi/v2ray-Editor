bdaa5d911a5d2f69f47480db400e19190c0872e4
Reza Aghajani <rezaaghajani@protonmail.com>
2023-04-04 03:14:25 +0330

    Add VLESS/TROJAN support to telegram bot

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ const token = process.env.TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+var domains = ["mci.ircf.space","mtn.ircf.space","rtl.ircf.space","mkh.ircf.space","hwb.ircf.space","ast.ircf.space","sht.ircf.space","prs.ircf.space","mbt.ircf.space","rsp.ircf.space","ztl.ircf.space","psm.ircf.space","smt.ircf.space"]
+var names = ["Hamrah","Irancell", "Rightel", "Mokhaberat", "Hiweb", "Asiatech", "Shatel", "Pars Online", "MobinNet", "Respina", "Zitel", "Pishgaman", "Samantel"]
+
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
@@ -24,7 +27,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.onText(/\/config (.+)/, (msg, match) => {
+bot.onText(/\/vmess (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1]
 
@@ -32,8 +35,6 @@ bot.onText(/\/config (.+)/, (msg, match) => {
     var bytes = base64.decode(configg);
     var text = utf8.decode(bytes);
     var list = JSON.parse(text);
-    var domains = ["mci.ircf.space","mtn.ircf.space","rtl.ircf.space","mkh.ircf.space","hwb.ircf.space","ast.ircf.space","sht.ircf.space","prs.ircf.space","mbt.ircf.space","rsp.ircf.space","ztl.ircf.space","psm.ircf.space","smt.ircf.space"]
-    var names = ["Hamrah","Irancell", "Rightel", "Mokhaberat", "Hiweb", "Asiatech", "Shatel", "Pars Online", "MobinNet", "Respina", "Zitel", "Pishgaman", "Samantel"]
 
     var count = 0
 
@@ -93,3 +94,116 @@ bot.onText(/\/config (.+)/, (msg, match) => {
         count++
     }
 });
+
+
+bot.onText(/\/vless (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]
+
+  for (i in domains) {
+    var configg = resp.replace(/@(.*?):/gm,"@"+domains[i]+':')
+
+    switch (count) {
+			case 0:
+			  text2 = "همراه اول";
+			  break;
+			case 1:
+			  text2 = "ایرانسل";
+			  break;
+			case 2:
+			  text2 = "رایتل";
+			  break;
+			case 3:
+			  text2 = "مخابرات";
+			  break;
+			case 4:
+			  text2 = "های وب";
+			  break;
+			case 5:
+			  text2 = "آسیاتک";
+			  break;
+			case 6:
+			  text2 = "شاتل";
+			  break;
+			case 7:
+			  text2 = "پارس آنلاین";
+			  break;
+			case 8:
+			  text2 = "مبین نت";
+			  break;
+			case 9:
+			  text2 = "رسپینا";
+			  break;
+			case 10:
+			  text2 = "زیتل";
+			  break;
+			case 11:
+			  text2 = "پیشگامان";
+			  break;
+			case 12:
+			  text2 = "سامانتل";
+			  break;
+		  }
+
+      bot.sendMessage(chatId, text2+": \n" + configg + " \n\n");
+      count++
+
+
+  }
+
+
+})
+
+bot.onText(/\/trojan (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]
+
+  for (i in domains) {
+    var configg = resp.replace(/@(.*?):/gm,"@"+domains[i]+':')
+
+    switch (count) {
+			case 0:
+			  text2 = "همراه اول";
+			  break;
+			case 1:
+			  text2 = "ایرانسل";
+			  break;
+			case 2:
+			  text2 = "رایتل";
+			  break;
+			case 3:
+			  text2 = "مخابرات";
+			  break;
+			case 4:
+			  text2 = "های وب";
+			  break;
+			case 5:
+			  text2 = "آسیاتک";
+			  break;
+			case 6:
+			  text2 = "شاتل";
+			  break;
+			case 7:
+			  text2 = "پارس آنلاین";
+			  break;
+			case 8:
+			  text2 = "مبین نت";
+			  break;
+			case 9:
+			  text2 = "رسپینا";
+			  break;
+			case 10:
+			  text2 = "زیتل";
+			  break;
+			case 11:
+			  text2 = "پیشگامان";
+			  break;
+			case 12:
+			  text2 = "سامانتل";
+			  break;
+		  }
+
+      bot.sendMessage(chatId, text2+": \n" + configg + " \n\n");
+      count++
+  }
+})
